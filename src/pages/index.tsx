19e22ce252e7fd25470bb0df193b57cd8225d919
Junior marques <jrmarqueshd@gmail.com>
2022-04-16 18:19:43 -0300

    import icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import { Text } from 'styles/typograph';
+import { Text, Heading } from 'styles/typograph';
 
 export default function Home() {
   return (
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-      laboriosam optio earum aliquam rerum accusantium quas, commodi quo
-      deserunt placeat nobis a? Ipsa, repudiandae quos quasi adipisci blanditiis
-      debitis quod!
-    </Text>
+    <>
+      <Heading>Titulo feliz e elegante</Heading>
+
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
+        laboriosam optio earum aliquam rerum accusantium quas, commodi quo
+        deserunt placeat nobis a? Ipsa, repudiandae quos quasi adipisci
+        blanditiis debitis quod!
+      </Text>
+    </>
   );
 }
